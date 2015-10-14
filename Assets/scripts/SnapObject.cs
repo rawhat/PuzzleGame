f5bb0f470dbaed6065dc44de64827a5638e73b21
rawhat <me@rawhat.net>
2015-10-14 18:04:58 -0400

    basic functionality additions
    click and drag functionality added; snapping script included but not
    integrated yet

---
+++ Assets/scripts/SnapObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class SnapObject : MonoBehaviour {
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.CompareTag("RoadPiece"))
+        {
+            obj.gameObject.GetComponent<BlockController>().enabled = false;
+            Vector2 currPieceDimensions = this.transform.localScale;
+            Vector2 nextPieceDimensions = obj.transform.localScale;
+            Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y + (currPieceDimensions.y / 2) + (nextPieceDimensions.y / 2));
+            obj.transform.DOMove(targetPosition, 1f);
+        }
+    }
+}
