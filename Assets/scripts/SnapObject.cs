e338b0774930376bf2f9f6344df384f3665e95e7
Matt Walker <mjw355@drexel.edu>
2015-10-14 19:41:20 -0400

    multiple blocks now stack on left panel.

--- Assets/scripts/SnapObject.cs
+++ Assets/scripts/SnapObject.cs
@@ -4,7 +4,8 @@ using DG.Tweening;
 
 public class SnapObject : MonoBehaviour {
 
-    private bool blockMoved = false;
+   private bool blockMoved = false;
+   public int blocksAdded = 1;
 
     void OnTriggerStay(Collider obj)
     {
@@ -14,10 +15,21 @@ public class SnapObject : MonoBehaviour {
             Destroy(obj.gameObject.GetComponent<BlockController>());
             Destroy(obj.gameObject.GetComponent<BlockFallMover>());
             obj.gameObject.transform.DOKill();
-            Vector2 currPieceDimensions = this.transform.localScale;
+            Vector2 currPieceDimensions = blocksAdded * this.transform.localScale;
             Vector2 nextPieceDimensions = obj.transform.localScale;
             Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y + (currPieceDimensions.y / 2) + (nextPieceDimensions.y / 2), 1f);
+            blocksAdded+=1;
             obj.transform.DOMove(targetPosition, 1f);
+            obj.tag = "Untagged";
+            increaseTriggerHeight();
+            blockMoved = false;
         }
+
+    }
+
+    void increaseTriggerHeight()
+    {
+        BoxCollider trigger = GetComponent<BoxCollider>();
+        trigger.center = new Vector3(0f, blocksAdded * 0.5f, 0f);
     }
 }
