4a35a8db6daf00edab2bf4dd54ed58a7cd3dc1d2
rawhat <me@rawhat.net>
2015-10-14 23:25:44 -0400

    fixed fall speed to linear

--- Assets/scripts/BlockFallMover.cs
+++ Assets/scripts/BlockFallMover.cs
@@ -11,7 +11,7 @@ public class BlockFallMover : MonoBehaviour {
 
 	void Start () {
         panelBottom = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 1f));
-        transform.DOMove(new Vector3(transform.position.x, panelBottom.y, 1f), Random.Range(minSpeed, maxSpeed)).OnComplete(()=>Destroy(gameObject));
+        transform.DOMove(new Vector3(transform.position.x, panelBottom.y, 1f), Random.Range(minSpeed, maxSpeed)).SetEase(Ease.Linear).OnComplete(()=>Destroy(gameObject));
 	}
 	
 }
